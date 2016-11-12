c5e1e2500d72e5a54418a575ab4214b5fadf3b80
Steve <ascended121@gmail.com>
2016-11-12 13:40:58 -0500

    added interface for RFD900 (will read an interpret RSSI reports), updated MARS1 and MARS2 commands and telemetry for deadman operations

--- lib/interface_rfd900.rb
+++ lib/interface_rfd900.rb
@@ -4,48 +4,20 @@ require 'int_fcn.rb'
 
 
 module Cosmos
-class InterfaceCcsdsLink < SerialInterface
+class InterfaceRfd900 < SerialInterface
   def pre_write_packet(packet)
   
     # packet.buffer appears to be a string and is hard to manipulate
     data = packet.buffer
     
-    # if the packet is not a link message, prepend a LINK command to foward
-    # the message to the appropriate payload
-    if(!isLinkPkt(data))
-      puts "Found non-link packet!"
-      
-      # update the packet length for the LINK forward command
-      data = update_ccsds_length(data)
-    
-      # update the packet checksum for the LINK forward command
-      data = update_ccsds_checksum(data)
-      
-      # prepend the link command to forward the data if its not a link command
-      data = prepend_fwdmsgcmd(data)
-      
-    else
-      puts "Found link packet!"
-    end
-   
-    # update the packet length for the packet being sent
-    data = update_ccsds_length(data)
-    
-    # update the packet checksum for the packet being sent
-    data = update_ccsds_checksum(data)
-            
     p data
+    p data.class
     
     return data
   end
 
   def post_read_data(packet_data)
-
-    # loop through the packet to calculate the checksum
-    #packet_data.each_byte {|x| calc_checksum ^= x }
-    p "Received: "
-    p packet_data
-    
+        
     if packet_data.include?("pkts") && packet_data.include?("txe") && packet_data.include?("rxe") && packet_data.include?("stx")
 
       nodeid = ["%02x" % packet_data[/\[(\d+)\] pkts: (\d+) txe=(\d+) rxe=(\d+) stx=(\d+) srx=(\d+) ecc=(\d+)\/(\d+) temp=(\d+) dco=(\d+)/,1].to_i].pack("H*")
@@ -78,7 +50,7 @@ class InterfaceCcsdsLink < SerialInterface
     else
       return packet_data
     end
-    return packet_data
+    
   end
 
 end
